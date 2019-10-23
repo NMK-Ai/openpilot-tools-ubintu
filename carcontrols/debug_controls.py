06835189f2c2fb44645a81e9cf710715ab539f89
Riccardo <riccardo.biasini@gmail.com>
2019-10-23 16:29:35 -0700

    Fixes to debug controls after ignition logic has changed

--- carcontrols/debug_controls.py
+++ carcontrols/debug_controls.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python
 import struct
-import time
 from common.numpy_fast import clip
+from common.params import Params
 from copy import copy
-from cereal import car
+from cereal import car, log
 import selfdrive.messaging as messaging
 from selfdrive.car.car_helpers import get_car
 from selfdrive.boardd.boardd import can_list_to_can_capnp
+
+HwType = log.HealthData.HwType
 
 
 def steer_thread():
   poller = messaging.Poller()
 
   logcan = messaging.sub_sock('can')
+  health = messaging.sub_sock('health')
   joystick_sock = messaging.sub_sock('testJoystick', conflate=True, poller=poller)
 
   carstate = messaging.pub_sock('carState')
@@ -22,18 +25,21 @@ def steer_thread():
   button_1_last = 0
   enabled = False
 
-  CI, CP = get_car(logcan, sendcan)
+  # wait for health and CAN packets
+  hw_type = messaging.recv_one(health).health.hwType
+  has_relay = hw_type in [HwType.blackPanda, HwType.uno]
+  print("Waiting for CAN messages...")
+  messaging.get_one_can(logcan)
+
+  CI, CP = get_car(logcan, sendcan, has_relay)
+  Params().put("CarParams", CP.to_bytes())
 
   CC = car.CarControl.new_message()
-  joystick = messaging.recv_one(joystick_sock)
 
   while True:
 
     # send
-    for socket in poller.poll(0):
-      if socket is joystick_sock:
-        joystick = messaging.recv_one(socket)
-
+    joystick = messaging.recv_one(joystick_sock)
     can_strs = messaging.drain_sock_raw(logcan, wait_for_one=True)
     CS = CI.update(CC, can_strs)
 
@@ -41,29 +47,30 @@ def steer_thread():
     # the other.
     actuators = car.CarControl.Actuators.new_message()
 
-    axis_3 = clip(-joystick.testJoystick.axes[3] * 1.05, -1., 1.)          # -1 to 1
-    actuators.steer = axis_3
-    actuators.steerAngle = axis_3 * 43.   # deg
-    axis_1 = clip(-joystick.testJoystick.axes[1] * 1.05, -1., 1.)          # -1 to 1
-    actuators.gas = max(axis_1, 0.)
-    actuators.brake = max(-axis_1, 0.)
+    if joystick is not None:
+      axis_3 = clip(-joystick.testJoystick.axes[3] * 1.05, -1., 1.)          # -1 to 1
+      actuators.steer = axis_3
+      actuators.steerAngle = axis_3 * 43.   # deg
+      axis_1 = clip(-joystick.testJoystick.axes[1] * 1.05, -1., 1.)          # -1 to 1
+      actuators.gas = max(axis_1, 0.)
+      actuators.brake = max(-axis_1, 0.)
 
-    pcm_cancel_cmd = joystick.testJoystick.buttons[0]
-    button_1 = joystick.testJoystick.buttons[1]
-    if button_1 and not button_1_last:
-      enabled = not enabled
+      pcm_cancel_cmd = joystick.testJoystick.buttons[0]
+      button_1 = joystick.testJoystick.buttons[1]
+      if button_1 and not button_1_last:
+        enabled = not enabled
 
-    button_1_last = button_1
+      button_1_last = button_1
 
-    #print "enable", enabled, "steer", actuators.steer, "accel", actuators.gas - actuators.brake
+      #print "enable", enabled, "steer", actuators.steer, "accel", actuators.gas - actuators.brake
 
-    hud_alert = 0
-    audible_alert = 0
-    if joystick.testJoystick.buttons[2]:
-      audible_alert = "beepSingle"
-    if joystick.testJoystick.buttons[3]:
-      audible_alert = "chimeRepeated"
-      hud_alert = "steerRequired"
+      hud_alert = 0
+      audible_alert = 0
+      if joystick.testJoystick.buttons[2]:
+        audible_alert = "beepSingle"
+      if joystick.testJoystick.buttons[3]:
+        audible_alert = "chimeRepeated"
+        hud_alert = "steerRequired"
 
     CC.actuators.gas = actuators.gas
     CC.actuators.brake = actuators.brake
@@ -87,10 +94,6 @@ def steer_thread():
     cc_send.init('carControl')
     cc_send.carControl = copy(CC)
     carcontrol.send(cc_send.to_bytes())
-
-
-    # Limit to 100 frames per second
-    time.sleep(0.01)
 
 
 if __name__ == "__main__":
